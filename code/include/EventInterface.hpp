fe9389b3a45f510a71c44f3efb475c36b156303e
Leigh Garbs <craymen@gmail.com>
2014-10-12 15:15:19 -0400

    Initial hack at abstract EventInterface class.  EventType typedef'd as an unsigned long long to guarantee at least 8 bytes of size to accomodate 64-bit memory addresses.

---
+++ code/include/EventInterface.hpp
@@ -0,0 +1,31 @@
+#if !defined EVENT_INTERFACE_HPP
+#define EVENT_INTERFACE_HPP
+
+#include <sstream>
+
+#include "EventType.hpp"
+
+/* Abstract base class for all events */
+class EventInterface
+{
+public:
+
+  EventInterface();
+
+  ~EventInterface();
+
+  /* Derived classes implement this to return a unique identifier for the type
+     of event they represent */
+  virtual EventType getEventType() const = 0;
+  
+  /* Returns the time when the event was created? Triggered? */
+  /*virtual double getTimeStamp() const = 0;*/
+
+  /* Formats class contents into a text stream */ 
+  /*virtual serialize(const std::ostream& o) const = 0;*/
+
+  /* Returns a copy of this event */
+  /*virtual EventDataInterface* copy() const = 0;*/
+};
+
+#endif
