21c1f1a7873c1d5c8713275890042a1cac77da7e
jpszabo <jpszabo@email.wm.edu>
2014-10-19 14:56:44 -0400

    First hack at map. Currently static 8x6 2D array to hold map. No sprites yet.

---
+++ code/CMakeFiles/helloworld.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/bin/helloworld.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/bin/helloworld.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/Actor.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/Actor.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/ActorMovedEvent.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/ActorMovedEvent.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/EventInterface.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/EventInterface.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/EventManager.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/EventManager.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/GameLogic.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/GameLogic.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/GameView.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/GameView.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/HumanGameView.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/HumanGameView.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/Map.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/Map.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/Port.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/Port.cpp.o"
+  "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/src/Ship.cpp" "/Users/localszabo/Documents/CS420/new_git/cs420-game/code/CMakeFiles/helloworld.dir/src/Ship.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Library/Frameworks/SFML.framework"
+  "include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ code/CMakeFiles/helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld.dir/src/Actor.cpp.o"
+  "CMakeFiles/helloworld.dir/src/ActorMovedEvent.cpp.o"
+  "CMakeFiles/helloworld.dir/src/EventInterface.cpp.o"
+  "CMakeFiles/helloworld.dir/src/EventManager.cpp.o"
+  "CMakeFiles/helloworld.dir/src/GameLogic.cpp.o"
+  "CMakeFiles/helloworld.dir/src/GameView.cpp.o"
+  "CMakeFiles/helloworld.dir/src/HumanGameView.cpp.o"
+  "CMakeFiles/helloworld.dir/src/Map.cpp.o"
+  "CMakeFiles/helloworld.dir/src/Port.cpp.o"
+  "CMakeFiles/helloworld.dir/src/Ship.cpp.o"
+  "CMakeFiles/helloworld.dir/bin/helloworld.cpp.o"
+  "helloworld.pdb"
+  "helloworld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
