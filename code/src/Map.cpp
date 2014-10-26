284a5561fb41d3c56ec019611a9276a7475a90d3
jpszabo <jpszabo@email.wm.edu>
2014-10-26 15:38:49 -0400

    Addressed Issue #15 of Map Coordinate inconsistency. All map functions deal in terms of x,y, no longer in terms of row,col.

--- code/src/Map.cpp
+++ code/src/Map.cpp
@@ -15,21 +15,21 @@ bool Map::initialize()
 
 bool Map::createMap(std::string filename) {
     std::string line;
-    int row = 0;
+    int y = 0;
     std::ifstream map_file (filename);
     if (map_file.is_open())
     {
         while (std::getline (map_file,line) ) 
         {
-            for (int col = 0; col < map_cols; col++) {
-                if (line[col] == 'L') {
-                    map_array[row][col] = LAND;
+            for (int x = 0; x < map_size_x; x++) {
+                if (line[x] == 'L') {
+                    map_array[x][y] = LAND;
                 }
-                else if (line[col] == 'W') {
-                    map_array[row][col] = WATER;
+                else if (line[x] == 'W') {
+                    map_array[x][y] = WATER;
                 }
             }
-            row++;
+            y++;
         }
         return true;
     }
@@ -44,11 +44,11 @@ void Map::drawMap(sf::RenderWindow* App) {
     int x_position = 0;
     int y_position = 0;
     sf::RectangleShape tile(sf::Vector2f(tile_size,tile_size));
-    for (int row = 0; row < map_rows; row++) 
+    for (int y = 0; y < map_size_y; y++) 
     {
-        for (int col = 0; col < map_cols; col++) 
+        for (int x = 0; x < map_size_x; x++) 
         {
-            if (map_array[row][col] == LAND)
+            if (map_array[x][y] == LAND)
             {
                 tile.setFillColor(sf::Color::Green);
             }
