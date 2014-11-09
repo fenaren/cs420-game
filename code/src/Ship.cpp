798539533b9048e58da98f33fbab005318aa5379
Christine Shen <lifesurfeit@gmail.com>
2014-11-09 11:36:34 -0500

    fixed rum rates to update within port and ship update functions

--- code/src/Ship.cpp
+++ code/src/Ship.cpp
@@ -7,7 +7,7 @@ Ship::Ship(ActorId actor_id) :
   Actor(actor_id),
   gold(0.0),
   rum(0.0),
-  max_rum(1.0),
+  max_rum(10.0),
   rum_rate(1.0)
 {
 }
@@ -24,4 +24,5 @@ bool Ship::initialize()
 void Ship::update(const sf::Time& delta_t)
 {
   Actor::update(delta_t);
+  rum -= rum_rate*delta_t;
 }
