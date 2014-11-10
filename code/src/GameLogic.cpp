0e9eefcceefef2e37ad16fbead8da72162dd6534
jpszabo <jpszabo@email.wm.edu>
2014-11-10 15:59:19 -0500

    Final commit. GameLogic now allows ship to move along beaches. Outdated enum of LAND and WATER removed from Map.hpp.

--- code/src/GameLogic.cpp
+++ code/src/GameLogic.cpp
@@ -208,7 +208,7 @@ void GameLogic::ShipMoveCmdEventHandler(const EventInterface& event)
 
   // does the move if target terrain is water (this ship can only move on water)
   // AND if enough time has passed
-  if (map.getTerrain(new_pos_x, new_pos_y) == Map::WATER &&
+  if (map.getTerrain(new_pos_x, new_pos_y) != 19 &&
       new_pos_x < map.get_map_size_x() &&
       new_pos_y < map.get_map_size_y() &&
       ship->getMoveTime() > ship->getMinMoveTime())
