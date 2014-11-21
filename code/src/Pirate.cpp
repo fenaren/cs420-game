833ed17d4841f5f17667dab7d984cedcc09b7b72
Justin Smith <jel150@gmail.com>
2014-11-20 20:31:14 -0500

    Added Pirate.cpp

---
+++ code/src/Pirate.cpp
@@ -0,0 +1,26 @@
+#include "Pirate.hpp"
+#include <iostream>
+
+Pirate::Pirate(ActorId actor_id) : EnemyActor(actor_id)
+{
+	setType(EnemyActor::Pirate);
+	setState(EnemyActor::Patrol);
+	setSeek(sf::Vector2i(getPositionX(), getPositionY()));
+	setPrevPos(getSeek());
+	setRumPenalty(2);
+	setMinMoveTime(1.0);
+	setAggroRange(6);
+}
+
+void Pirate::checkState(sf::Vector2i ship_pos) {
+	if (checkAggroRange(ship_pos)) {
+		setState(EnemyActor::Pursue);
+		setMinMoveTime(.75);
+		setNeedSeek(true);
+	}
+	else if (getState() == EnemyActor::Pursue) {
+		setState(EnemyActor::Patrol);
+		setMinMoveTime(1.0);
+		setNeedSeek(true);
+	}
+}
